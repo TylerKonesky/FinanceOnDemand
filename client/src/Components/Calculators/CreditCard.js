5e3f2671e8539d6c7c1b92fd4fcd9af9a4ec29fe
TylerKonesky <tylerkonesky@hotmail.com>
2020-05-02 17:48:19 -0600

    more calculator

--- client/src/Components/Calculators/CreditCard.js
+++ client/src/Components/Calculators/CreditCard.js
@@ -1,39 +1,45 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const CreditCard = props =>{
-    let interest = 0;
-    let testData = [
-        {
-            balance: 1000,
-            interest: 14.99,
-            monthlyPayment: 50,
-            months: 0,
-            totalInterest: 0
-        },
-        {
-            balance: 500,
-            interest: 7.99,
-            monthlyPayment: 50,
-            months: 0,
-            totalInterest: 0
-        },
-        {
-            balance: 2000,
-            interest: 10.99,
-            monthlyPayment: 50,
-            months: 0,
-            totalInterest: 0
-        },
-        {
-            balance: 5000,
-            interest: 21.99,
-            monthlyPayment: 250,
-            months: 0,
-            totalInterest: 0
-        },
-    ]
+export default class CreditCard extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            interest: 0,
+            testData: [
+                {
+                    balance: 1000,
+                    interest: 14.99,
+                    monthlyPayment: 50,
+                    months: 0,
+                    totalInterest: 0
+                },
+                {
+                    balance: 500,
+                    interest: 7.99,
+                    monthlyPayment: 50,
+                    months: 0,
+                    totalInterest: 0
+                },
+                {
+                    balance: 2000,
+                    interest: 10.99,
+                    monthlyPayment: 50,
+                    months: 0,
+                    totalInterest: 0
+                },
+                {
+                    balance: 5000,
+                    interest: 21.99,
+                    monthlyPayment: 250,
+                    months: 0,
+                    totalInterest: 0
+                },
+            ],
+            cards: []
+        }
+    }
 
-    function caluclation(testData){
+    caluclation(testData){
         testData.map(card =>{
             while(card.balance > 0){
                 card.balance -= card.monthlyPayment;
@@ -45,13 +51,13 @@ const CreditCard = props =>{
            
         })
         testData.map(card =>{
-            interest += card.totalInterest;
+            this.state.interest += card.totalInterest;
         })
         return testData;
     }
 
-    const renderCalculations = () =>{
-        const calc = caluclation(testData);
+    renderCalculations = () =>{
+        const calc = this.caluclation(this.state.testData);
         return calc.map(card =>{
             return(
                 <div>
@@ -63,15 +69,36 @@ const CreditCard = props =>{
             )
         })
     }
+    renderCards(){
+        return this.state.cards.map(card =>{
+            return(
+                <div>
+                    <label>Card</label>
+                    <input label="Card"></input>
+                </div>
+            )
+        })
+    }
 
-
-
-
-    return(
-        <div>
-            {renderCalculations()}
-        </div>
-    )
+    addCard(){
+        
+        this.setState({
+            cards: [...this.state.cards, {card: '', balance: 0, interest: 0, monthlyPayment: 0, months: 0, totalInterest: 0}]
+        })
+        console.log(this.state.cards)
+        
+    }
+    render(){
+        return(
+            <div className="container">
+                {this.renderCards()}
+                
+                <button onClick={()=>{this.addCard()}}>Add Card</button>
+               
+                
+                
+                {/* {this.renderCalculations()} */}
+            </div>
+        )
+    }
 }
-
-export default CreditCard
